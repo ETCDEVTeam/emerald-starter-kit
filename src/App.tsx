74e1c2eb8c0f6099086ed26ffde33184e1be4814
shanejonas <jonas.shane@gmail.com>
2018-08-26 23:50:45 -0700

    feature: add new todos contract

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import Input from "emerald-js-ui/lib/components/Input";
-import Page from "emerald-js-ui/lib/components/Page";
 import * as React from "react";
 
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import { TransactionButton } from 'emerald-js-ui';
-
-import Paper from "@material-ui/core/Paper";
-import { Contract, EmeraldProvider, AppBar, EtcBalance, NetworkSelector, AccountSelector, CurrentBlockNumber } from 'emerald-js-ui';
+import { Paper, Checkbox, List, ListItem, ListItemText, ListItemIcon} from "@material-ui/core";
+import { Page, Input, TransactionButton, Contract, EmeraldProvider, AppBar, EtcBalance, NetworkSelector, AccountSelector, CurrentBlockNumber } from 'emerald-js-ui';
 
 const contractJson = require("../build/contracts/Todos.json");
 
@@ -58,18 +51,36 @@ class App extends React.Component<{}, IAppState> {
     });
   }
 
-  public renderTodos(todos) {
+  public renderTodos(todoIds) {
     return (
       <List component="nav">
-        {todos.map((todo, i) => {
-          return (
+      {todoIds.map((todoId) => {
+        return (
+          <Contract address={this.state.contractAddress} abi={this.state.contractAbi} method="getTodo" params={{index: todoId.toNumber()}}>
+          {([text, complete, deleted]) => (
             <Paper>
-              <ListItem key={i}>
-                <ListItemText primary={new Buffer(todo, 'hex').toString()} />
+              <ListItem key={todoId.toNumber()}>
+                <ListItemIcon>
+                  <Checkbox checked={complete.value}/>
+                </ListItemIcon>
+                <ListItemText primary={new Buffer(text.value, 'hex').toString()} />
+                <TransactionButton transaction={{
+                  ...this.state.transaction,
+                  mode: "contract_function",
+                  functionSignature: this.state.contractAbi.find((item) => item.name === 'toggleTodoAtIndex'),
+                  argsDefaults: [
+                    {
+                      name: "index",
+                      value: todoId.toNumber()
+                    },
+                  ]
+                }} />
               </ListItem>
             </Paper>
-          );
-        })}
+          )}
+          </Contract>
+        );
+      })}
       </List>
     );
   }
@@ -83,7 +94,7 @@ class App extends React.Component<{}, IAppState> {
         functionSignature: this.state.contractAbi.find((item) => item.name === 'addTodo'),
         argsDefaults: [
           {
-            name: "todo",
+            name: "todoText",
             value: event.target.value
           },
         ]
@@ -106,8 +117,8 @@ class App extends React.Component<{}, IAppState> {
             <Input multiline={true} id="textarea" value={this.state.textarea} onChange={this.handleTextAreaChange.bind(this)}/>
             <TransactionButton transaction={this.state.transaction} />
           </div>
-          <Contract address={this.state.contractAddress} abi={this.state.contractAbi} method="getTodos" refresh={3000}>
-            {(results) => this.renderTodos(results[0].value)}
+          <Contract address={this.state.contractAddress} abi={this.state.contractAbi} method="getTodoIds" refresh={3000}>
+            {([{value}]) => this.renderTodos(value)}
           </Contract>
         </Page>
       </EmeraldProvider>
